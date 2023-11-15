3ea64a67522f3c1ab8d98db223937843aacfddc0
anudeepyalamanchi <anudeepyalamanchi143@gmail.com>
2023-11-14 20:11:41 -0600

    PATTERNS PROJECT
    DMS

---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,13 @@
+/*
+ * @author Naga Anudeep Yalamanchi
+ */
+
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
